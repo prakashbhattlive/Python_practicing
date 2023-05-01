658e06e3f24baaead9a498953d4a4a7fd09c3891
Prakash Bhatt <prakashbhatt@live.com>
2023-05-01 23:59:14 +0530

    updating json

--- Python_Beg/10th_hour_json.py
+++ Python_Beg/10th_hour_json.py
@@ -5,3 +5,20 @@ d={
 }
 j=json.dumps(d)
 print(j, type(j))
+
+d1= '{"cname" : "python", "fees" : 3000, "duration" : "2 months"}'
+x1=json.loads(d1)
+print(x1, type(x1))
+
+for a in x1:
+    print(a, x1[a])
+
+####Writing & reading Json
+
+file2=open('nifty_data.json','r')
+x2=file2.read()
+final_data=json.loads(x2)
+
+for a in final_data:
+    #print(a)
+    print(a['HistoricalDate'], a['OPEN'])    
